57facfdd4b02f48499764298dab76b717450ce22
PRANAV8966 <pranav.wan706@gmail.com>
2025-06-26 11:43:52 +0530

    expense frontend

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { createUser, getUser, getAllUser, signIn }  = require('../controllers/user-controller.js');
 const { create, destroy, getAll } = require('../controllers/expense-controller.js');
 
-router.post('/signUp',createUser);
+
 
 router.get('/getUser', getUser);
 router.get('/getAllUsers', getAllUser);
@@ -12,6 +12,7 @@ router.get('/getAllUsers', getAllUser);
 router.get('/getAllExpenses', getAll);
 
 router.post('/signIn', signIn);
+router.post('/signUp',createUser);
 
 router.post('/createExpense', create);
 
